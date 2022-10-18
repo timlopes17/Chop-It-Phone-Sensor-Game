1c9d5850d678dd1768a304c9ef3035b8255f1de3
timlopesTU <tug92082@temple.edu>
2022-10-18 15:56:43 -0400

    Funtional Game Activity

--- app/src/main/java/edu/temple/chopitgame/GameActivity.kt
+++ app/src/main/java/edu/temple/chopitgame/GameActivity.kt
@@ -6,18 +6,32 @@ import android.media.SoundPool
 import android.media.metrics.Event
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 import android.provider.MediaStore
 import android.util.Log
+import android.view.View
+import android.widget.ImageView
+import android.widget.TextView
 import android.widget.Toast
 import kotlin.concurrent.fixedRateTimer
 import kotlin.concurrent.thread
 import kotlin.random.Random
+import android.os.Looper
+
+
+
 
 class GameActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_game)
+
+        val button = findViewById<ImageView>(R.id.button);
+        val layout = findViewById<View>(R.id.layout)
+        val butWidth = button.layoutParams.width
+        val butHeight = button.layoutParams.height
+        val text = findViewById<TextView>(R.id.text)
 
         var background = MediaPlayer.create(this, R.raw.background)
         background.start()
@@ -44,6 +58,8 @@ class GameActivity : AppCompatActivity() {
             for(n in 0..49){
                 speed = 600F/timeBetween + 0.8F
                 event = events[Random.nextInt(0, 6)]
+                Handler(Looper.getMainLooper()).post(Runnable { text.text = event.title.toString() })
+                //text.text = event.title
                 soundPool?.play(event.soundId, 1F, 1F, 0, 0, speed)
                 Log.d("Game", event.title)
                 Log.d("Game", "Speed: $speed")
