97c13da47bfc3adf9c46b351694f876cdc6a06a5
timlopesTU <tug92082@temple.edu>
2022-11-17 09:14:18 -0500

    2 Moves now working, Chop It and Flip It. Also added sounds for getting the correct move

--- app/src/main/java/edu/temple/chopitgame/GameActivity.kt
+++ app/src/main/java/edu/temple/chopitgame/GameActivity.kt
@@ -25,6 +25,8 @@ import kotlin.random.Random
 import android.os.Looper
 import android.util.TypedValue
 import org.w3c.dom.Text
+import kotlin.math.max
+import kotlin.math.min
 import kotlin.math.roundToInt
 
 
@@ -33,13 +35,31 @@ class GameActivity : AppCompatActivity(), SensorEventListener {
     private lateinit var sensorManager: SensorManager
     private lateinit var sensorAccel : Sensor
     private lateinit var sensorMagnet : Sensor
+    private lateinit var sensorLight : Sensor
 
     private var accelData = FloatArray(3)
     private var magnetData = FloatArray(3)
+    private var lightData = FloatArray(1)
 
     private lateinit var pitchText : TextView
     private lateinit var azimuthText : TextView
     private lateinit var rollText : TextView
+    private lateinit var lightText : TextView
+
+    private var minPitch : Double = 180.0
+    private var maxPitch : Double = -180.0
+    private var minRoll : Double = 180.0
+    private var maxRoll : Double = -180.0
+    private var minAzimuth : Double = 180.0
+    private var maxAzimuth : Double = -180.0
+
+    private var record : Int = -1
+    private var n: Int = 0
+    private var size : Float = 80F
+    private lateinit var soundPool : SoundPool
+    private var correctID : Int = 0
+    private lateinit var background : MediaPlayer
+    var doubleMove = 0
 
     private val drift = 0.05f
 
@@ -54,11 +74,11 @@ class GameActivity : AppCompatActivity(), SensorEventListener {
         val text = findViewById<TextView>(R.id.text)
         val score = findViewById<TextView>(R.id.score)
 
-        var background = MediaPlayer.create(this, R.raw.background)
+        background = MediaPlayer.create(this, R.raw.background)
         background.start()
         var timeBetween : Long = 3000
 
-        var soundPool : SoundPool = SoundPool.Builder().build()
+        soundPool = SoundPool.Builder().build()
 
         val events = arrayOf(
             EventObject("Click It", soundPool.load(baseContext, R.raw.click, 1)),
@@ -69,28 +89,32 @@ class GameActivity : AppCompatActivity(), SensorEventListener {
             EventObject("Spin It", soundPool.load(baseContext, R.raw.spin, 1)),
         )
 
+        correctID = soundPool.load(baseContext, R.raw.correct, 1)
+
         var event : EventObject
         var speed : Float
-        var size : Float = 80F
 
         sensorManager = getSystemService((Context.SENSOR_SERVICE)) as SensorManager
         sensorAccel = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER)
         sensorMagnet = sensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD)
+        sensorLight = sensorManager.getDefaultSensor(Sensor.TYPE_LIGHT)
 
         azimuthText = findViewById(R.id.a_val)
         pitchText = findViewById(R.id.p_val)
         rollText = findViewById(R.id.r_val)
+        lightText = findViewById(R.id.l_val)
 
-        thread(start = true){
+        var oldN : Int
+
+        thread(start = true) {
             Thread.sleep(500)
-            for(n in 0..49){
-                speed = 600F/timeBetween + 0.8F
-                event = events[Random.nextInt(0, 6)]
-                size += 1F
+            while (n != 50 && n != -17) {
+                oldN = n
+                speed = 600F / timeBetween + 0.8F
+                event = events[1]
+                //event = events[Random.nextInt(0, 6)]
                 Handler(Looper.getMainLooper()).post(Runnable {
                     text.text = event.title.toString()
-                    score.text = n.toString()
-                    score.setTextSize(TypedValue.COMPLEX_UNIT_SP, size)
                 })
                 soundPool?.play(event.soundId, 1F, 1F, 0, 0, speed)
                 //Log.d("Game", event.title)
@@ -98,27 +122,71 @@ class GameActivity : AppCompatActivity(), SensorEventListener {
                 //Log.d("Game", "TimeBetween: $timeBetween")
                 //Log.d("Game", "Textsize: ${score.textSize}")
                 Thread.sleep(500)
-                event = events[3]
-                if(event.title == "Flip It")
-                {
-
+                if (event.title == "Flip It" || event.title == "Chop It" ||
+                    event.title == "Twist It" || event.title == "Spin It") {
+                    sensorManager.registerListener(
+                        this,
+                        sensorAccel,
+                        SensorManager.SENSOR_DELAY_NORMAL
+                    )
+                    sensorManager.registerListener(
+                        this,
+                        sensorMagnet,
+                        SensorManager.SENSOR_DELAY_NORMAL
+                    )
+                    if(event.title == "Flip It")
+                        record = 3
+                    if(event.title == "Chop It")
+                        record = 1
                 }
-                Thread.sleep(timeBetween - 500)
+                Thread.sleep(5000 + timeBetween - 500)
                 timeBetween -= 10
+                if (n == oldN)
+                    Log.d("Game", "YOU LOSE")
             }
+            soundPool.release()
         }
+    }
+
+    fun correct() {
+        val score = findViewById<TextView>(R.id.score)
+        n += 1
+        size += 1F
+        soundPool.play(correctID, 0.5F, 0.5F, 0, 0, 1F)
+        Handler(Looper.getMainLooper()).post(Runnable {
+            score.text = n.toString()
+            score.setTextSize(TypedValue.COMPLEX_UNIT_SP, size)
+        })
+        Log.d("Game", "CORRECT")
+        record = -1
+        minPitch = 180.0
+        maxPitch = -180.0
+        minRoll = 180.0
+        maxRoll = -180.0
+        minAzimuth = 180.0
+        maxAzimuth = -180.0
+    }
+
+    fun lose() {
+        val sPool : SoundPool = SoundPool.Builder().build()
+        var sound = sPool.load(baseContext, R.raw.wrong, 1)
+        sPool.play(sound, 1F, 1F, 0, 0, 1F)
+        n = -17
+        record = -1
+        Log.d("Game", "LOSE")
     }
 
     override fun onStart() {
         super.onStart()
         sensorManager.registerListener(this, sensorAccel, SensorManager.SENSOR_DELAY_NORMAL)
         sensorManager.registerListener(this, sensorMagnet, SensorManager.SENSOR_DELAY_NORMAL)
+        sensorManager.registerListener(this, sensorLight, SensorManager.SENSOR_DELAY_NORMAL)
     }
 
     override fun onStop() {
         super.onStop()
-
         sensorManager.unregisterListener(this)
+        background.release()
     }
 
     override fun onSensorChanged(event: SensorEvent?) {
@@ -131,25 +199,69 @@ class GameActivity : AppCompatActivity(), SensorEventListener {
             Sensor.TYPE_MAGNETIC_FIELD -> {
                 magnetData = event!!.values.clone()
             }
+            Sensor.TYPE_LIGHT -> {
+                lightData = event!!.values.clone()
+            }
             else -> {
                 return
             }
         }
+        if(record == 1 || record == 3 || record == 4 || record == 5)
+        {
+            var rotationMatrix = FloatArray(9)
+            var rotOK = SensorManager.getRotationMatrix(rotationMatrix, null, accelData, magnetData)
+            var orientationValues = FloatArray(3)
 
-        var rotationMatrix = FloatArray(9)
-        var rotOK = SensorManager.getRotationMatrix(rotationMatrix, null, accelData, magnetData)
-        var orientationValues = FloatArray(3)
+            if(rotOK)
+                SensorManager.getOrientation(rotationMatrix, orientationValues)
 
-        if(rotOK)
-            SensorManager.getOrientation(rotationMatrix, orientationValues)
+            var azimuth = orientationValues[0] * 57.3
+            var pitch = orientationValues[1] * 57.3
+            var roll = orientationValues[2] * 57.3
 
-        var azimuth = orientationValues[0]
-        var pitch = orientationValues[1]
-        var roll = orientationValues[2]
+            azimuthText.text = azimuth.toString()
+            pitchText.text = pitch.toString()
+            rollText.text = roll.toString()
 
-        azimuthText.text = azimuth.toString()
-        pitchText.text = pitch.toString()
-        rollText.text = roll.toString()
+            minPitch = min(pitch, minPitch)
+            maxPitch = max(pitch, maxPitch)
+            minRoll = min(roll, minRoll)
+            maxRoll = max(roll, maxRoll)
+            minAzimuth = min(azimuth, minAzimuth)
+            maxAzimuth = max(azimuth, maxAzimuth)
+            //Chop
+            if(record == 1){
+                if(doubleMove == 1 && maxRoll - minRoll >= 90 && maxAzimuth - minAzimuth >= 90){
+                    correct()
+                    doubleMove = 0
+                }
+                else if(maxRoll - minRoll >= 90 && maxAzimuth - minAzimuth >= 90){
+                    doubleMove = 1
+                    maxRoll = roll
+                    minRoll = roll
+                }
+            }
+            //Flip
+            if(record == 3){
+                if(maxPitch - minPitch >= 60){
+                    correct()
+                }
+            }
+            //Twist
+            if(record == 4){
+
+            }
+            //Spin
+            if(record == 5){
+
+            }
+        }
+        else if(record == 2){
+            //Light
+            var lx = lightData[0]
+
+            lightText.text = lx.toString()
+        }
     }
 
     override fun onAccuracyChanged(p0: Sensor?, p1: Int) {
